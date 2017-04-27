d76a763b71317bff1b0f3e15a828f3af5cc20c7e
Tomas Korcak <korczis@gmail.com>
2017-04-27 14:13:44 -0400

    Refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::fs::{self, DirEntry};
+use std::io;
+use std::path::Path;
+use std::sync::mpsc;
+
 pub enum MessageType {
     Entry,
     Exit,
@@ -44,8 +53,117 @@ impl FsDirInfo {
     }
 }
 
+pub type FsStack = Vec<FsDirInfo>;
+
 #[derive(Debug, Copy, Clone)]
 pub struct OverallInfo {
     pub dirs: u64,
     pub files: u64,
+}
+
+pub type RxChannel = mpsc::Receiver<(MessageType, Option<OverallInfo>, Option<String>)>;
+pub type TxChannel = mpsc::Sender<(MessageType, Option<OverallInfo>, Option<String>)>;
+
+pub fn get_file_info(path: &Path, entry: &DirEntry) -> FsFileInfo {
+    let md = Box::new(entry.metadata().unwrap()) as Box<std::os::unix::fs::MetadataExt>;
+
+    let res = FsFileInfo {
+        path: path.to_str().unwrap().to_string(),
+        ino: md.ino(),
+        mtime: md.mtime(),
+        size: md.size(),
+    };
+
+    debug!("{:?}", res);
+
+    return res;
+}
+
+pub fn process(tx: TxChannel, dirs: &Vec<&str>) {
+    let mut info = OverallInfo {
+        dirs: 0,
+        files: 0,
+    };
+
+    let mut dir_stack: FsStack = FsStack::new();
+    let mut visitor = |path: &Path, _entry: &DirEntry| {
+        if path.is_dir() {
+            info.dirs += 1;
+        } else if path.is_file() {
+            info.files += 1;
+
+            let _ = tx.send((MessageType::Entry,
+                             Some(info.clone()),
+                             Some(path.to_str().unwrap().to_string())));
+        }
+    };
+
+    for dir in dirs.iter() {
+        let _ = self::visit_dir(&mut dir_stack, Path::new(dir), &mut visitor);
+    }
+}
+
+pub fn visit_dir(stack: &mut FsStack,
+                 dir: &Path,
+                 cb: &mut FnMut(&Path, &DirEntry))
+                 -> io::Result<()> {
+
+    debug!("Entering directory {:?}", dir);
+
+    let mut stat = OverallInfo {
+        dirs: 0,
+        files: 0,
+    };
+
+    let metadata = fs::symlink_metadata(dir)?;
+    let file_type = metadata.file_type();
+
+    if dir.is_dir() && !file_type.is_symlink() {
+        // let _dir_meta = dir.metadata();
+        let dir_info = FsDirInfo {
+            path: dir.to_str().unwrap().to_string(),
+            // dirs: Vec::new(),
+            files: Vec::new(),
+            files_size: 0,
+        };
+
+        stack.push(dir_info);
+
+        let mut dir_files = Vec::new();
+
+        if let Ok(entries) = fs::read_dir(dir) {
+            for entry in entries {
+                debug!("Processing entry {:?}", entry);
+
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_dir() {
+                        debug!("Found directory: {:?}", &path);
+
+                        stat.dirs += 1;
+                        let _ = self::visit_dir(stack, &path, cb);
+                        cb(&path, &entry);
+                    } else if path.is_file() {
+                        debug!("Found file: {:?}", &path);
+
+                        stat.files += 1;
+
+                        dir_files.push(self::get_file_info(&path, &entry));
+                        cb(&path, &entry);
+                    }
+                }
+            }
+        }
+
+        stack.last_mut().unwrap().files = dir_files;
+        stack.last_mut().unwrap().calculate_files_size();
+
+        debug!("{:?}", stack);
+        stack.pop();
+    } else {
+        debug!("{:?}", stack);
+    }
+
+    debug!("Leaving directory {:?}", dir);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-#[macro_use]
 extern crate log;
 extern crate env_logger;
 
 extern crate clap;
 extern crate wims;
+extern crate time;
 
 use clap::{App, Arg};
-use std::env;
 use std::io;
 use std::io::Write;
-use std::fs::{self, DirEntry};
-use std::path::Path;
+use std::env;
 use std::sync::mpsc;
 use std::thread;
-use std::time::SystemTime;
+use time::PreciseTime;
 use wims::*;
 
 const AUTHOR: &'static str = env!("CARGO_PKG_AUTHORS");
@@ -21,8 +19,6 @@ const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let mut stdout = io::stdout();
-
     let matches = App::new(DESCRIPTION)
         .version(VERSION)
         .author(AUTHOR)
@@ -69,8 +65,50 @@ fn main() {
     let progress_format =
         ProgressFormat::from(matches.value_of("progress-format").unwrap().to_string());
 
+    fn print_stats(info: &OverallInfo,
+                   elapsed_secs: f64,
+                   progress: bool,
+                   progress_format: ProgressFormat) {
+
+        let dirs_count = info.dirs;
+        let files_count = info.files;
+
+        let fpd = if dirs_count > 0 {
+            files_count / dirs_count
+        } else {
+            0
+        };
+
+        let fps = if elapsed_secs > 0.0 {
+            (files_count as f64 / elapsed_secs) as u64
+        } else {
+            0
+        };
+
+        if progress {
+            match progress_format {
+                ProgressFormat::Dot => println!(""),
+                _ => {}
+            };
+        }
+
+        println!("Dirs: {}, Files: {}, Files Per Dir: {}, Time: {}, Speed: {} fps",
+                 dirs_count,
+                 files_count,
+                 fpd,
+                 elapsed_secs,
+                 fps);
+    }
+
+    let mut stdout = io::stdout();
     let (tx, rx) = mpsc::channel();
+    let start = PreciseTime::now();
     let handle = thread::spawn(move || {
+        let mut info = OverallInfo {
+            files: 0,
+            dirs: 0,
+        };
+
         loop {
             match rx.recv() {
                 Ok(received) => {
@@ -78,7 +116,7 @@ fn main() {
 
                     match data.0 {
                         MessageType::Entry => {
-                            let info = data.1.unwrap();
+                            info = data.1.unwrap();
                             let path = data.2.unwrap();
 
                             if progress && (info.files % progress_count) == 0 {
@@ -90,6 +128,12 @@ fn main() {
                             }
                         }
                         MessageType::Exit => {
+                            let diff = start.to(PreciseTime::now());
+                            let elapsed_secs = diff.num_seconds() as f64 +
+                                               diff.num_milliseconds() as f64 * 0.001 +
+                                               diff.num_microseconds().unwrap() as f64 * 1e-6;
+
+                            print_stats(&info, elapsed_secs, progress, progress_format);
                             break;
                         }
                     };
@@ -99,154 +143,9 @@ fn main() {
         }
     });
 
-    let mut info = OverallInfo {
-        dirs: 0,
-        files: 0,
-    };
+    let dirs: Vec<_> = matches.values_of("DIR").unwrap().collect();
+    wims::process(tx.clone(), &dirs);
 
-    let now = SystemTime::now();
-    {
-        type FsStack = Vec<FsDirInfo>;
-
-        let mut dir_stack: FsStack = FsStack::new();
-
-        fn get_file_info(path: &Path, entry: &DirEntry) -> FsFileInfo {
-            let md = Box::new(entry.metadata().unwrap()) as Box<std::os::unix::fs::MetadataExt>;
-
-            let res = FsFileInfo {
-                path: path.to_str().unwrap().to_string(),
-                ino: md.ino(),
-                mtime: md.mtime(),
-                size: md.size(),
-            };
-
-            debug!("{:?}", res);
-
-            return res;
-        };
-
-        // one possible implementation of walking a directory only visiting files
-        fn visit_dir(stack: &mut FsStack,
-                     dir: &Path,
-                     cb: &mut FnMut(&Path, &DirEntry))
-                     -> io::Result<()> {
-
-            debug!("Entering directory {:?}", dir);
-
-            let mut stat = OverallInfo {
-                dirs: 0,
-                files: 0,
-            };
-
-            let metadata = fs::symlink_metadata(dir)?;
-            let file_type = metadata.file_type();
-
-            if dir.is_dir() && !file_type.is_symlink() {
-                // let _dir_meta = dir.metadata();
-                let dir_info = FsDirInfo {
-                    path: dir.to_str().unwrap().to_string(),
-                    // dirs: Vec::new(),
-                    files: Vec::new(),
-                    files_size: 0,
-                };
-
-                stack.push(dir_info);
-
-                let mut dir_files = Vec::new();
-
-                if let Ok(entries) = fs::read_dir(dir) {
-                    for entry in entries {
-                        debug!("Processing entry {:?}", entry);
-
-                        if let Ok(entry) = entry {
-                            let path = entry.path();
-                            if path.is_dir() {
-                                debug!("Found directory: {:?}", &path);
-
-                                stat.dirs += 1;
-                                let _ = visit_dir(stack, &path, cb);
-                                cb(&path, &entry);
-                            } else if path.is_file() {
-                                debug!("Found file: {:?}", &path);
-
-                                stat.files += 1;
-
-                                dir_files.push(get_file_info(&path, &entry));
-                                cb(&path, &entry);
-                            }
-                        }
-                    }
-                }
-
-                stack.last_mut().unwrap().files = dir_files;
-                stack.last_mut().unwrap().calculate_files_size();
-
-                debug!("{:?}", stack);
-                stack.pop();
-            } else {
-                debug!("{:?}", stack);
-            }
-
-            debug!("Leaving directory {:?}", dir);
-            Ok(())
-        }
-
-        let mut visitor = |path: &Path, _entry: &DirEntry| {
-            if path.is_dir() {
-                info.dirs += 1;
-            } else if path.is_file() {
-                info.files += 1;
-
-                let _ = tx.send((MessageType::Entry,
-                                 Some(info.clone()),
-                                 Some(path.to_str().unwrap().to_string())));
-            }
-        };
-
-        let dirs: Vec<_> = matches.values_of("DIR").unwrap().collect();
-        for dir in dirs.iter() {
-            let _ = visit_dir(&mut dir_stack, Path::new(dir), &mut visitor);
-        }
-    }
-
-    let elapsed = now.elapsed();
-    match elapsed {
-        Ok(elapsed) => {
-            let dirs_count = info.dirs;
-            let files_count = info.files;
-
-            let elapsed_secs = (elapsed.as_secs() as f64) +
-                               ((elapsed.subsec_nanos() as f64) * 1e-9);
-
-            let fpd = if dirs_count > 0 {
-                files_count / dirs_count
-            } else {
-                0
-            };
-
-            let fps = if elapsed_secs > 0.0 {
-                (files_count as f64 / elapsed_secs) as u64
-            } else {
-                0
-            };
-
-            if progress {
-                match progress_format {
-                    ProgressFormat::Dot => println!(""),
-                    _ => {}
-                };
-            }
-
-            let _ = tx.send((MessageType::Exit, None, None));
-            let _ = handle.join();
-
-            println!("Dirs: {}, Files: {}, Files Per Dir: {}, Time: {}, Speed: {} fps",
-                     dirs_count,
-                     files_count,
-                     fpd,
-                     elapsed_secs,
-                     fps);
-        }
-        _ => {}
-    };
+    let _ = tx.send((MessageType::Exit, None, None));
+    let _ = handle.join();
 }
